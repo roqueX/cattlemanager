ff20daad130d0c0a3cce11e41722f68975da8cb5
Roque Cruz <roque.cruz.acosta@gmail.com>
2011-08-20 16:42:27 -0500

    inicio de proyecto 20110820

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cattlemanager::Application.config.secret_token = '51601bd088c21004b1833173e1215f79bcc7b7d63104620a456ea537eb275d84e787d231aa269e64d72bbe3f606d9063115365529703349e46a7d239e54bad04'
